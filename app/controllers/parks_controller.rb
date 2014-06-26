3a17e36c260247a16c84b21f3830ca07338244af
charlyy <cyoon90@yahoo.com>
2014-06-26 05:58:30 -0700

    added parks controller

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,9 @@
+class ParksController < ApplicationController
+  def index
+  end
+
+  def search
+    parameters = { term: params[:term], limit: 16 }
+    render json: Yelp.client.search('San Francisco', parameters)
+  end
+end
