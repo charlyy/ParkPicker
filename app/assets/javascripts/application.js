a8d075747d1842ea0a5fc3aeb6f49d709a52f6ba
charlyy <cyoon90@yahoo.com>
2014-06-26 07:00:22 -0700

    added the google maps javascript initializer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,44 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+var markersArray = [];
+var SM_LAT = 34.0219;
+var SM_LNG = -118.4814;
+var QUERY_DELAY = 400;
+var inactive = false;
+
+$(document).ready(function() {
+  // initialize the map on load
+  initialize();
+});
+
+/**
+ * Initializes the map and some events on page load
+ */
+var initialize = function() {
+  // Define some options for the map
+  var mapOptions = {
+    center: new google.maps.LatLng(SM_LAT, SM_LNG),
+    zoom: 12,
+
+    // hide controls
+    panControl: false,
+    streetViewControl: false,
+
+    // reconfigure the zoom controls
+    zoomControl: true,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_BOTTOM,
+      style: google.maps.ZoomControlStyle.SMALL
+    }
+  };
+
+  // create a new Google map with the options in the map element
+  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+  bind_controls(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
